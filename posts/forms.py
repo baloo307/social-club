8747acd4cdeda0b004619bfd79501e86dd5c277e
baloo307 <76101213+baloo307@users.noreply.github.com>
2020-12-19 03:43:37 +0900

    Update forms.py

--- posts/forms.py
+++ posts/forms.py
@@ -7,12 +7,3 @@ class PostForm(forms.ModelForm):
         fields = ("message", "group")
         model = models.Post
 
-    def __init__(self, *args, **kwargs):
-        user = kwargs.pop("user", None)
-        super().__init__(*args, **kwargs)
-        if user is not None:
-            self.fields["group"].queryset = (
-                models.Group.objects.filter(
-                    pk__in=user.groups.values_list("group__pk")
-                )
-            )
