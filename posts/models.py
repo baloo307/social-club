2bcfca92233f475a5795cb4d5ade1baaa6748744
baloo307 <76101213+baloo307@users.noreply.github.com>
2020-12-19 02:52:11 +0900

    Update models.py

--- posts/models.py
+++ posts/models.py
@@ -12,7 +12,6 @@ class Post(models.Model):
     user = models.ForeignKey(User, related_name="posts",on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now=True)
     message = models.TextField()
-    message_html = models.TextField(editable=False)
     group = models.ForeignKey(Group, related_name="posts",null=True, blank=True,on_delete=models.CASCADE)
 
     def __str__(self):
