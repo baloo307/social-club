4837e503c229b0f5221e699a04b06245efb0d360
baloo307 <76101213+baloo307@users.noreply.github.com>
2020-12-19 01:26:48 +0900

    Update models.py

--- groups/models.py
+++ groups/models.py
@@ -12,7 +12,6 @@ class Group(models.Model):
     name = models.CharField(max_length=255, unique=True)
     slug = models.SlugField(allow_unicode=True, unique=True)
     description = models.TextField(blank=True, default='')
-    description_html = models.TextField(editable=False, default='', blank=True)
     members = models.ManyToManyField(User,through="GroupMember")
 
     def __str__(self):
